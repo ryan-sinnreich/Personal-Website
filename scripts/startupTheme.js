bf49113471295c91f5c385d91f6245b28fb39770
Ryan Sinnreich <47066669+ryan-sinnreich@users.noreply.github.com>
2025-09-01 17:37:45 -0500

    Fixes

--- scripts/startupTheme.js
+++ scripts/startupTheme.js
@@ -1,8 +1,15 @@
 // This script is run right after the body tag of each .html file to make sure the correct theme is applied
-(function() {
-      const theme = localStorage.getItem('theme');
+(function () {
+  const lightThemeColor = '#90EE90';
+  const darkThemeColor = '#16A34A';
 
-      if (theme === 'dark') {
-        document.body.classList.add('dark-mode');
-      }
-    })();
+  const theme = localStorage.getItem('theme');
+  const themeColorMeta = document.getElementById('theme-color-meta');
+
+  if (theme === 'dark') {
+    document.body.classList.add('dark-mode');
+    themeColorMeta.setAttribute('content', darkThemeColor);
+  } else {
+    themeColorMeta.setAttribute('content', lightThemeColor);
+  }
+})();
